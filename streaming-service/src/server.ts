a1ef09e996d01e00a0b72455ead5594c150119ef
adrianbalbs <adrianbalbs@hotmail.com>
2023-02-27 23:52:30 +1100

    Refactored error handling for server.ts

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -15,19 +15,20 @@ tcpServer.on('connection', (socket) => {
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        try {
-          let currJSON = JSON.parse(msg.toString());
-          addToLog(currJSON);
-          websocketServer.clients.forEach(function each(client) {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(msg.toString());
-              }
-            });
-        } catch (err) {
-          socket.on('error', (err) => {
-              console.log('TCP client error: ', err);
-          });
-        }
+          try {
+              let currJSON = JSON.parse(msg.toString());
+              addToLog(currJSON);
+              websocketServer.clients.forEach(function each(client) {
+                  if (client.readyState === WebSocket.OPEN) {
+                    client.send(msg.toString());
+                  }
+                });
+          } catch (err) {
+            console.error('Invalid JSON Parsed');
+          }
+      });
+    socket.on('error', (err) => {
+        console.log('TCP client error: ', err);
     });
     socket.on('end', () => {
         console.log('Closing connection with the TCP client');
