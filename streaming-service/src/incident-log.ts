aca463d8ef4a99433d6bdbc3806daa2120b26176
adrianbalbs <adrianbalbs@hotmail.com>
2023-02-28 12:10:27 +1100

    Installed and configured eslint

--- streaming-service/src/incident-log.ts
+++ streaming-service/src/incident-log.ts
@@ -1,36 +1,36 @@
 import fs from 'fs';
 
 interface msg {
-  "battery_temperature": number;
-  "timestamp": number;
+  'battery_temperature': number;
+  'timestamp': number;
 }
 
 const timeLog: msg[] = [];
-let incidentCount: number = 0;
+let incidentCount = 0;
 
 // Takes the message and adds it to the timeLog array, also checks if temperatures exceed 80 more than three times 
 export function addToLog(msg: msg) {
-  if (timeLog.length !== 0 && msg.timestamp >= timeLog[0].timestamp + 5100){
-    // console.log(timeLog);
-    checkTimeLog(msg);
-    timeLog.length = 0; 
-  } else {
-    timeLog.push(msg);
-  }
+	if (timeLog.length !== 0 && msg.timestamp >= timeLog[0].timestamp + 5100){
+		// console.log(timeLog);
+		checkTimeLog(msg);
+		timeLog.length = 0; 
+	} else {
+		timeLog.push(msg);
+	}
 }
 
 // Checks if temperatures exceed more than 80 three times, then write to incidents.log
 function checkTimeLog (msg: msg) {
-  for (msg of timeLog) {
-    if (msg.battery_temperature > 80 || msg.battery_temperature < 20) {
-      incidentCount++;
-      // console.log(incidentCount);
-    }
-    if (incidentCount > 3) {
-      const incident: string = `Log: Timestamp of Incident: ${timeLog.pop()?.timestamp}`;
-      fs.writeFileSync('./incidents.log', incident);
-      break;
-    } 
-  }
-  incidentCount = 0;
+	for (msg of timeLog) {
+		if (msg.battery_temperature > 80 || msg.battery_temperature < 20) {
+			incidentCount++;
+			// console.log(incidentCount);
+		}
+		if (incidentCount > 3) {
+			const incident = `Log: Timestamp of Incident: ${timeLog.pop()?.timestamp}`;
+			fs.writeFileSync('./incidents.log', incident);
+			break;
+		} 
+	}
+	incidentCount = 0;
 }
--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -8,43 +8,41 @@ const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
 
 tcpServer.on('connection', (socket) => {
-    console.log('TCP client connected');
+	console.log('TCP client connected');
     
-    socket.on('data', (msg) => {
-        console.log(msg.toString());
-        // HINT: what happens if the JSON in the received message is formatted incorrectly?
-        // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        try {
-            let currJSON = JSON.parse(msg.toString());
-            addToLog(currJSON);
-            websocketServer.clients.forEach(function each(client) {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(msg.toString());
-                }
-              });
-        } catch (err) {
-            console.error("Unexpected token error");
-            return err;
-        }
-    });
-    socket.on('error', (err) => {
-        console.log('TCP client error: ', err);
-    });
-    socket.on('end', () => {
-        console.log('Closing connection with the TCP client');
-    });
+	socket.on('data', (msg) => {
+		console.log(msg.toString());
+		// HINT: what happens if the JSON in the received message is formatted incorrectly?
+		// HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+		try {
+			const currJSON = JSON.parse(msg.toString());
+			addToLog(currJSON);
+			websocketServer.clients.forEach(function each(client) {
+				if (client.readyState === WebSocket.OPEN) {
+					client.send(msg.toString());
+				}
+			});
+		} catch (err) {
+			console.error('Unexpected token error');
+			return err;
+		}
+	});
+	socket.on('error', (err) => {
+		console.log('TCP client error: ', err);
+	});
+	socket.on('end', () => {
+		console.log('Closing connection with the TCP client');
+	});
     
 });
 
 websocketServer.on('listening', () => console.log('Websocket server started'));
 
 websocketServer.on('connection', async (ws: WebSocket) => {
-    console.log('Frontend websocket client connected to websocket server');
-    ws.on('error', console.error);  
+	console.log('Frontend websocket client connected to websocket server');
+	ws.on('error', console.error);  
 });
 
 tcpServer.listen(TCP_PORT, () => {
-    console.log(`TCP server listening on port ${TCP_PORT}`);
+	console.log(`TCP server listening on port ${TCP_PORT}`);
 });
-
-
